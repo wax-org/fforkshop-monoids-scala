b6a8c60f2bf93e34903f523c4e8346b90c4c5add
Boris Buliga <boris@d12frosted.io>
2019-10-30 16:58:59 +0200

    properly check for fib implementation

--- src/test/scala/wax/exercise/fibonacci/FibSpec.scala
+++ src/test/scala/wax/exercise/fibonacci/FibSpec.scala
@@ -1,22 +1,33 @@
 package wax.exercise.fibonacci
 
-import cats.tests.CatsSuite
 import org.scalacheck.{Gen, Prop}
+import org.scalacheck.Properties
+import org.scalacheck.Prop._
 
-class FibSpec extends CatsSuite {
+import scala.collection.mutable.ListBuffer
+
+object FibSpec extends Properties("Fibonacci") {
 
   fibIsFibProp("tailRecFib", Fib.fibTailRec)
   fibIsFibProp("matrixFib", Fib.fibOtEn)
 
   val enGen: Gen[BigInt] = Gen.choose(2, 1000).map(a => BigInt(a))
-  def fibIsFibProp(name: String, f: BigInt => BigInt): Unit =
-    test(s"$name is fib") {
-      Prop.forAll(enGen) { en: BigInt =>
+
+  def fibIsFibProp(name: String, f: BigInt => BigInt): ListBuffer[(String, Prop)] = {
+    property("fib(0) == 0") = {
+      f(0) =? 0
+    }
+    property("fib(1) == 1") = {
+      f(1) =? 1
+    }
+    property(s"$name is fib") = {
+      forAll(enGen) { en: BigInt =>
         val a = f(en - 2)
         val b = f(en - 1)
         val c = f(en)
         a + b == c
-      }.check
+      }
     }
+  }
 
 }
